f45941f7fabe9736b16f65dee9f8900a9f3f320b
Lenhazada <gabriellenhardtcontato@gmail.com>
2026-01-23 18:14:02 -0300

    Adicionando comentarios na classe DslistApplication

--- src/main/java/com/devsuperior/dslist/DslistApplication.java
+++ src/main/java/com/devsuperior/dslist/DslistApplication.java
@@ -3,6 +3,12 @@ package com.devsuperior.dslist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe principal da aplicação.
+ *
+ * Responsável por inicializar o Spring Boot
+ * e iniciar o contexto da aplicação.
+ */
 @SpringBootApplication
 public class DslistApplication {
 
