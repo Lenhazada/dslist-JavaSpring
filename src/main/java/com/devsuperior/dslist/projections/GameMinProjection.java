ea2368b3402488b6582d066b1e950b7de8a6377a
Lenhazada <gabriellenhardtcontato@gmail.com>
2026-01-23 17:57:37 -0300

    Adicionando comentarios em projections

--- src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -1,5 +1,13 @@
 package com.devsuperior.dslist.projections;
 
+/**
+ * Projection utilizada para retornar apenas os dados essenciais do jogo.
+ *
+ * É usada para otimizar consultas ao banco de dados,
+ * evitando carregar todas as informações da entidade Game
+ * quando apenas um subconjunto de campos é necessário.
+ */
+
 public interface GameMinProjection {
 	Long getId();
 	String getTitle();
